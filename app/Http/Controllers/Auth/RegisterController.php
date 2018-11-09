65f3215ef02adcd747a0253d453d5bc8fbad7b43
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2018-11-09 13:53:47 +0800

    Add registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
 
 class RegisterController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'email|required|unique:users,email',
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'email' => $request->email,
+            'name' => $request->name,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return $user;
+    }
 }
