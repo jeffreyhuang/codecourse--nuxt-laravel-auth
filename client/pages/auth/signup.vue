b9129c8ed2489d2e82a884d1038924869a37388a
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2018-11-15 16:46:00 +0800

    Add sign-up page and methods

--- client/pages/auth/signup.vue
+++ client/pages/auth/signup.vue
@@ -1,9 +1,76 @@
 <template>
-  <p>sign up</p>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <div class="column is-4 is-offset-4">
+          <h1 class="title has-text-grey has-text-centered">Join us</h1>
+
+          <div class="box">
+            <form method="" @submit.prevent="submit">
+              <div class="field">
+                <label for="email" class="label">Email address</label>
+                <div class="control">
+                  <input class="input is-medium" :class="{ 'is-danger': errors.email }" type="email" id="email" v-model="form.email">
+
+                  <p class="help is-danger" v-if="errors.email">{{ errors.email[0] }}</p>
+                </div>
+              </div>
+
+              <div class="field">
+                <label for="name" class="label">Full name</label>
+                <div class="control">
+                  <input class="input is-medium" :class="{ 'is-danger': errors.name }" id="name" v-model="form.name">
+
+                  <p class="help is-danger" v-if="errors.name">{{ errors.name[0] }}</p>
+                </div>
+              </div>
+
+              <div class="field">
+                <label for="password" class="label">Password</label>
+                <div class="control">
+                  <input class="input is-medium" :class="{ 'is-danger': errors.password }" type="password" id="password" v-model="form.password">
+
+                  <p class="help is-danger" v-if="errors.password">{{ errors.password[0] }}</p>
+                </div>
+              </div>
+
+              <div class="field">
+                <button class="button is-block is-info is-medium is-fullwidth">Sign up</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  //
+  data () {
+    return {
+      form: {
+        email: '',
+        name: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async submit () {
+      await this.$axios.post('register', this.form)
+
+      await this.$auth.login({
+        data: {
+          email: this.form.email,
+          password: this.form.password
+        }
+      })
+
+      this.$router.push({
+        name: 'index'
+      })
+    }
+  }
 }
 </script>
